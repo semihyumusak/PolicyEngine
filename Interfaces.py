bdcbff297ddaf50a107e49a56a328943d64ee05e
Semih Yumuşak <semihyumusak@yahoo.com>
2024-04-04 10:19:55 +0100

    Update Interfaces.py
    create refinable interface

--- Interfaces.py
+++ Interfaces.py
@@ -7,14 +7,12 @@ Contributors:
 
 """
 
-
 from abc import abstractmethod, ABC
 
 from Constraint import Constraint
 
 
 class RefinableInterface(ABC):
-
     @abstractmethod
     def add_refinement(self, constraint: Constraint):
         pass
