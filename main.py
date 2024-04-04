acda20bc158c5da8fe23bea8cfbbacbc2ed86eed
Semih Yumuşak <semihyumusak@yahoo.com>
2024-04-04 10:23:21 +0100

    Update main.py
    sample web service for policy matching.

--- main.py
+++ main.py
@@ -7,19 +7,17 @@ Contributors:
 
 """
 
-
 from flask import Flask, request, jsonify
-import json
 from Constraint import Constraint
+from Parsers import ODRLParser
 
 app = Flask(__name__)
 
 # Assuming 'policy.odrl' is the file path
-file_path = 'policy.odrl'
-policy = ""
-with open(file_path, 'r') as file:
-    policy = json.load(file)
+file_path = "./examples/policy.odrl"
 
+odrl = ODRLParser()
+policy = odrl.parse(file_path)
 
 @app.route('/evaluate', methods=['POST'])
 def evaluate():
@@ -31,26 +29,27 @@ def evaluate():
     allowed = True
     reason = ""
 
-    for permission in policy.get("permission", []):
-        if permission["target"] == target and permission["action"] == action:
+    for permission in policy.permission:
+        if permission.target == target and permission.action == action:
             permission_found = True
-            if "constraint" in permission:
-                constraints = permission["constraint"] if isinstance(permission["constraint"], list) else [permission["constraint"]]
+            if len(permission.constraint)>0:
+                constraints = permission.constraint if isinstance(permission.constraint, list) else [permission.constraint]
                 if not Constraint.evaluate(constraints, context):
                     allowed = False
                     reason = "Constraint not satisfied for permission"
             if allowed:
-                for duty in permission.get("duty", []):
+                for duty in permission.duty:
                     if not duty.is_fulfilled(duty, context):
                         allowed = False
                         reason = "Duty not met"
                         break
 
     if allowed:
-        for prohibition in policy.get("prohibition", []):
-            if prohibition["target"] == target and prohibition["action"] == action:
-                if "constraint" in prohibition:
-                    constraints = prohibition["constraint"] if isinstance(prohibition["constraint"], list) else [prohibition["constraint"]]
+        for prohibition in policy.prohibition:
+            if prohibition.target == target and prohibition.action == action:
+                if len(prohibition.constraint)>0:
+                    constraints = prohibition.constraint if isinstance(prohibition.constraint, list) else [prohibition.constraint]
+                    # TODO: check constraints
                     if Constraint.evaluate(constraints, context, "or"):  # Assuming OR logic for prohibition constraints
                         allowed = False
                         reason = "Prohibition applies"
