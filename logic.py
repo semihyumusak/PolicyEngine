4898df38c03ac603d6c5b827dde2f1ff47eabbf6
Semih Yumuşak <semihyumusak@yahoo.com>
2024-05-03 14:05:50 +0100

    Update logic.py

--- logic.py
+++ logic.py
@@ -27,36 +27,43 @@ def extract_logic(policies):
     logic_op = conjunction
     y = 0
     for p in policies:
-        results = []
+        #results = []
         i = 1
         for proh in p.prohibition:
             id = f"P{i}"
+            query = ""
             i += 1
 #            id = f"\"{p.uid.split(':')[2]}\""
-            query = proh.target
-            query_parts = query.split("<--")
-            target = query_parts[0].split("(")[1].replace(")","")
-
+            for c in proh.constraint:
+                if c.leftOperand == "ex:query":
+                    query = c.rightOperand
+            if query == "":
+                query = proh.target
+            #query = proh.target
+            query_parts = query.split(":-")
+            q_name = query.split("(")[0]
+            inputs = query_parts[0].split("(")[1].replace(")","")
+            target = proh.target.split("/")[-1].split("#")[-1]
             pattern = r'Table\d+\([^)]+\)'
             tables = re.findall(pattern,  query_parts[1])
 
-            logic_expression += f"hasTarget({id}, {target})  {logic_op} "
+            logic_expression += f"hasTarget({q_name}, {inputs})  {logic_op} "
             for table in tables:
                 logic_expression += f"{table.strip()}  {logic_op} "
             if check(proh.assignee, actorTypes):
                 function_name = get(proh.assignee, actorTypes)
-                logic_expression += f"hasActor ({id},y{y}) {logic_op} {function_name} (y{y}) {logic_op} "
+                logic_expression += f"hasActor ({q_name},y{y}) {logic_op} {function_name} (y{y}) {logic_op} "
                 y += 1
             if check(proh.action, actionTypes):
                 function_name = get(proh.action, actionTypes)
-                logic_expression += f"hasAction ({id},y{y}) {logic_op} {function_name} (y{y}) {logic_op} "
+                logic_expression += f"hasAction ({q_name},y{y}) {logic_op} {function_name} (y{y}) {logic_op} "
                 y += 1
             for c in proh.constraint:
                 if check(c.rightOperand, purposeTypes):
                     function_name = get(c.rightOperand, purposeTypes)
-                    logic_expression += f"hasPurpose ({id},y{y}) {logic_op} {function_name} (y{y}) {logic_op} "
+                    logic_expression += f"hasPurpose ({q_name},y{y}) {logic_op} {function_name} (y{y}) {logic_op} "
                     y += 1
             logic_expression = logic_expression [:-2] + "\n\n"
-            results.append({"uid":id,"rule":logic_expression})
+            #results.append({"uid":id,"rule":logic_expression})
 
     return logic_expression
